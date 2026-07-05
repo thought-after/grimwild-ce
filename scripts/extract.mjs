f467f90c22d453f9935cca84490564956e3f5759
thought-after <halfwalkmedia@gmail.com>
2026-07-05 15:36:08 -0400

    sanitize the hanging "- "
    test

--- scripts/extract.mjs
+++ scripts/extract.mjs
@@ -1,8 +1,10 @@
 import { extractPack } from "@foundryvtt/foundryvtt-cli";
+import {rm} from "fs/promises";
 
 const packs = ["gwce-arcana", "gwce-talents", "gwce-journals", "gwce-crucibles", "gwce-actors"];
 
 for (const pack of packs) {
+  await rm(`src/${pack}`, {recursive: true, force: true});
   await extractPack(
     `packs/${pack}`,
     `src/${pack}`,
